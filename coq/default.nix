890f1c43b33a9c5c1f8c7c483afe0373b25464d8
Langston Barrett <langston.barrett@gmail.com>
2017-10-11 20:41:40 +0000

    coq: use HoTT from nixpkgs

--- coq/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import ./pinned-pkgs.nix { } }:
-
-# TODO: build my files :)
-
-pkgs.callPackage ./hott.nix { coq = pkgs.coq_8_6; }
--- coq/pinned-pkgs.nix
+++ coq/pinned-pkgs.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
 
+# This commit added HoTT to nixpkgs
 import (pkgs.fetchFromGitHub {
   owner  = "NixOS";
   repo   = "nixpkgs";
-  rev    = "17.09-beta";
-  sha256 = "1psff8lpp5kmihsg7xv46a0d3rmhsb4kyb9w8r213gn8ggigwg57";
+  rev    = "48a49fc12e0b6e28c46fcb0a7c52bac26f8bc05d";
+  sha256 = "1vxk0mi0qb6syv6afb04li0bvdgxg60dxi35xq1khcy71d214ch1";
 }) { }
